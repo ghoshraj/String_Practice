c0cc266443c3851880841e51f736c83c6692eab6
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-05 17:44:13 +0530

    First Commit

--- ChangeRepeatCharacter.java
+++ ChangeRepeatCharacter.java
@@ -6,6 +6,8 @@ public class ChangeRepeatCharacter {
         char[] ch = s1.toCharArray();
         for (int i = 0 ; i < ch.length; i++){
            int count = 0;
+           if (ch[i] == '$')
+               continue;
             for (int j = i +1; j < ch.length;j++){
                 if (ch[i] == ch[j]){
                     ch[j] = '$';
