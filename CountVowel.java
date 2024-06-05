1e5f88a777975a8bc07d4a68761c26f45072e1e4
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-05 18:01:27 +0530

    First Commit

--- CountVowel.java
+++ CountVowel.java
@@ -8,6 +8,7 @@ public class CountVowel {
         String s = sc.nextLine();
         int count = 0;
         for (int i = 0; i < s.length(); i++){
+            s = s.toLowerCase();
             if (s.charAt(i) == 'a'|| s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'){
                 count++;
             }
