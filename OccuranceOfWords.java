5e9c8871f536e21a369cf71f5aabb3785d824d31
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-07 11:40:17 +0530

    First Commit

--- OccuranceOfWords.java
+++ OccuranceOfWords.java
@@ -6,7 +6,7 @@ public class OccuranceOfWords {
         String []s2 = s1.split(" ");
         for (int i = 0; i < s2.length; i++) {
             int count = 1;
-            if (s2[i] == "\u0000") {
+            if (s2[i] .equals("\u0000")) {
             continue;
             }
             for (int j = i + 1; j < s2.length; j++) {
