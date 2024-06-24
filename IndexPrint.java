50b8cff248cc0a77751278f810ea58ab8252d0ed
rajesh <110840094+ghoshraj@users.noreply.github.com>
2024-06-24 22:40:00 +0530

    First Commit

--- IndexPrint.java
+++ IndexPrint.java
@@ -7,10 +7,10 @@ public class IndexPrint {
         for (int i = 0; i < ch.length; i++){
             if (ch[i] == '\u0000')
                 continue;
-            System.out.print(ch[i] + " = " + i + " ");
+            System.out.print(ch[i] + " = " + i);
             for (int j = i + 1; j < ch.length; j++) {
                 if (ch[i] == ch[j]) {
-                    System.out.print(j);
+                    System.out.print(" , " +j);
                     ch[j] = '\u0000';
                 }
             }
